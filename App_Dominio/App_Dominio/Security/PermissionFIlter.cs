0ae59a2000b5e9e2b0000cef7850376cbbefa39e
Andre Leal <andreborgesleal@live.com>
2013-11-29 18:27:38 -0300

    Atribute Roles

---
+++ App_Dominio/App_Dominio/Security/PermissionFIlter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace App_Dominio.Security
+{
+    public class PermissionFilter : AuthorizeAttribute
+    {
+        public override void OnAuthorization(AuthorizationContext filterContext)
+        {
+            base.OnAuthorization(filterContext);
+
+            // caso o usuário não seja autoriazado, ele será enviado para a página /Account/Login
+            if (filterContext.Result is HttpUnauthorizedResult)
+                filterContext.HttpContext.Response.Redirect("/Account/Login/");
+        }
+    }
+}
