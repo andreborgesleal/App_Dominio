6478a9d4452abd14186544686486edb2a5bd53ea
Andre Leal <andreborgesleal@live.com>
2013-12-09 09:00:50 -0300

    Ajustes no git

---
+++ App_Dominio/App_Dominio/Repositories/UsuarioRepository.cs
@@ -0,0 +1,45 @@
+﻿using App_Dominio.Component;
+using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace App_Dominio.Repositories
+{
+    public class UsuarioRepository : Repository
+    {
+        [DisplayName("ID")]
+        public decimal usuarioId { get; set; }
+
+        [DisplayName("Empresa ID")]
+        public int empresaId { get; set; }
+
+        [Required]
+        [DataType(DataType.EmailAddress, ErrorMessage = "Informe um e-mail válido")]
+        [EmailAddress]
+        [Display(Name = "Login")]
+        public string login { get; set; }
+
+        [DisplayName("Nome")]
+        [Required]
+        [StringLength(40, ErrorMessage="O nome do usuário deve ter no máximo 40 caracteres" )]
+        public string nome { get; set; }
+
+        public DateTime dt_cadastro { get; set; }
+
+        [Required]
+        [DisplayName("Situação")]
+        public string situacao { get; set; }
+
+        [Required]
+        [DisplayName("Administrador (S/N")]
+        public string isAdmin { get; set; }
+        
+        [DataType(DataType.Password)]
+        public string senha { get; set; }
+
+        public string keyword { get; set; }
+
+        public Nullable<DateTime> dt_keyword { get; set; }
+    }
+}
